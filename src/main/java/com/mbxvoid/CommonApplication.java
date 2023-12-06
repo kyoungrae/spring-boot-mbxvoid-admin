e86a2b4828cf3b9136fe9b025f23fe7122e945b9
go66go66 <myljk1025@gmail.com>
2023-12-07 02:30:24 +0900

    123123

--- src/main/java/com/mbxvoid/CommonApplication.java
+++ src/main/java/com/mbxvoid/CommonApplication.java
@@ -10,12 +10,12 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "com.mbxvoid")
 public class CommonApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CommonApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CommonApplication.class, args);
 	}
 
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(CommonApplication.class);
-    }
 }
